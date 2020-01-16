00ed78454097b3caab2dc9a8046660f903e22a1d
Rasel3415 <Rasel5777@gmail.com>
2020-01-15 22:32:42 -0500

    works

--- src/problemandsolutions/ArraySort.java
+++ src/problemandsolutions/ArraySort.java
@@ -1,7 +1,36 @@
 package problemandsolutions;
-
+import java.util.Scanner;
 public class ArraySort {
     // By using the bubble sort algorithm, write a Java program to sort an integer array of 10 elements in ascending.
 
+    public static void main(String []args) {
+        int num, x, j, temp;
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter the number of integers to sort:");
+        num = input.nextInt();
+
+        int array[] = new int[num];
+
+        System.out.println("Enter " + num + " integers: ");
+
+        for (x = 0; x < num; x++)
+            array[x] = input.nextInt();
+
+        for (x = 0; x < ( num - 1 ); x++) {
+            for (j = 0; j < num - x - 1; j++) {
+                if (array[j] > array[j+1])
+                {
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted list of integers:");
+
+        for (x = 0; x < num; x++)
+            System.out.println(array[x]);
+    }
 }
